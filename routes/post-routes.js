5939768cc85c5f72a95706e4a5eabc0631f2f38d
Taiwo Tejuosho <ttejuosho@outlook.com>
2020-05-16 15:41:46 -0500

    post form validation

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -4,7 +4,14 @@ const {check} = require('express-validator');
 
 module.exports = function(app) {
     app.get('/', postController.getHomePage);
+
     app.get('/newPost', Security.isLoggedIn , postController.newPostPage);
-    app.post('/newPost', Security.isLoggedIn , postController.createNewPost);
+
+    app.post('/newPost', [ 
+        check('postTitle').not().isEmpty().escape().withMessage('Post title is required'),
+        check('postDescription').not().isEmpty().escape().withMessage('Post description is required'),
+        check('postImage').not().isEmpty().escape().withMessage('Post image is required'),
+    ], Security.isLoggedIn , postController.createNewPost);
+
     app.get('/post/:postId', postController.getPost);
 }
