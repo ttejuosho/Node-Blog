02db2b383cd75be338b99675864365950c68d152
Taiwo Tejuosho <taiwotejuosho@TaiMBP18.hsd1.il.comcast.net>
2020-06-19 07:05:14 -0500

    home model

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,8 +1,34 @@
 const db = require("../models");
+const sequelize = require("sequelize");
 
 // Render home page
-exports.getHomePage = (req, res) => {
-    return res.render("index", {
-      title: "TaiBlog",
-    });
+exports.getHomePage = async (req, res) => {
+  // Get latest Post
+  // Get Most Viewed/Read Posts/ Featured Posts
+  await db.Post.findOne({
+    attributes: [sequelize.fn('MAX', sequelize.literal('viewCount'))],
+    //order: [['groupId', 'DESC'], ['value', 'DESC']] 
+  }).then((dbPost)=>{
+    console.log(dbPost.dataValues);
+  })
+    // return res.render("index", {
+    //   title: "TaiBlog",
+    // });
   };
+
+exports.getPostsByCategory = (req, res) => {
+  db.Post.findAll({
+    where: {
+      postCategory: req.params.category
+    },
+    order: [["viewCount", "DESC"]],
+  }).then((dbPost)=>{
+    if (dbPost !== null){
+      var hbsObject = { categoryView: true, posts: [] };
+      dbPost.forEach((post)=>{
+        hbsObject.posts.push(post.dataValues);
+      })
+      res.render("index", hbsObject);
+    }
+  })
+}
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -3,4 +3,5 @@ const Security = require('../services/security/security.js');
 
 module.exports = function(app) {
     app.get('/', homeController.getHomePage);
+    app.get('/c/:category', homeController.getPostsByCategory);
 }
